79f5133bcc9e13d48857bd5f6fd00ea3017595ad
Danielle Barda <dbarda@redhat.com>
2024-05-15 13:56:55 +0300

    Add vm_list_group_by_clusters module

---
+++ tests/unit/plugins/modules/test_vm_list_group_by_clusters.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+import sys
+import pytest
+
+from ansible_collections.vmware.vmware.plugins.modules import vm_list_group_by_clusters
+
+from .common.utils import (
+    AnsibleExitJson, ModuleTestCase, set_module_args,
+)
+
+pytestmark = pytest.mark.skipif(
+    sys.version_info < (2, 7), reason="requires python2.7 or higher"
+)
+
+
+class TestVMList(ModuleTestCase):
+
+    def __prepare(self, mocker):
+        init_mock = mocker.patch.object(vm_list_group_by_clusters.VmwareVMList, "__init__")
+        init_mock.return_value = None
+
+        vm_list_group_by_clusters.VmwareVMList.content = mocker.Mock()
+        vm_list_group_by_clusters.VmwareVMList.module = mocker.Mock()
+        vm_list_group_by_clusters.VmwareVMList.module.check_mode = False
+
+        vm_list_group_by_clusters.VmwareVMList.params = {
+            'detailed_vms': False,
+        }
+
+        list_of_vms = mocker.patch.object(vm_list_group_by_clusters.VmwareVMList, "get_vm_list_group_by_clusters")
+        list_of_vms.return_value = {}
+
+    def test_gather(self, mocker):
+        self.__prepare(mocker)
+
+        set_module_args(
+            hostname="127.0.0.1",
+            username="administrator@local",
+            password="123456",
+            add_cluster=False,
+        )
+
+        with pytest.raises(AnsibleExitJson) as c:
+            vm_list_group_by_clusters.main()
+
+        assert c.value.args[0]["changed"] is False
