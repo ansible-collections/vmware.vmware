0c996197e0a6ad8cc91af4a6749210b7aba84aab
Mike Morency <mikemorency93@gmail.com>
2024-07-30 09:57:13 -0400

    syntax

--- plugins/modules/vm_portgroup_info.py
+++ plugins/modules/vm_portgroup_info.py
@@ -66,7 +66,7 @@ vm_portgroup_info:
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.vmware.vmware.plugins.module_utils._vmware import PyVmomi
-from ansible_collections.vmware.vmware.plugins.module_utils import vmware_network
+from ansible_collections.vmware.vmware.plugins.module_utils import _vmware_network as vmware_network
 from ansible_collections.vmware.vmware.plugins.module_utils._vmware_rest_client import VmwareRestClient
 
 
