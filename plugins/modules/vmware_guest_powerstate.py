4088dae6b9c13c490120556858e77346ef0a257c
Orr Ben Galim <obengali@redhat.com>
2025-02-03 14:56:45 +0200

    restructured module using class methods

--- plugins/modules/vmware_guest_powerstate.py
+++ plugins/modules/vmware_guest_powerstate.py
@@ -22,16 +22,14 @@ options:
     datacenter:
         description:
             - The datacenter where the VM you'd like to operate the power.
-        default: ha-datacenter
         type: str
-        required: false
+        required: true
     state:
         description:
             - Set the state of the virtual machine.
         choices: [ powered-off, powered-on, reboot-guest, restarted, shutdown-guest, suspended, present]
         default: present
         type: str
-        required: false
     name:
         description:
             - Name of the virtual machine to work with.
@@ -98,7 +96,6 @@ options:
         description:
             - Flag to indicate whether the scheduled task is enabled or disabled.
         type: bool
-        required: false
         default: true
     force:
         description:
@@ -223,11 +220,146 @@ from ansible_collections.community.vmware.plugins.module_utils.vmware import PyV
     check_answer_question_status, make_answer_response, answer_question, gather_vm_facts
 from ansible.module_utils._text import to_native
 
+from ansible_collections.vmware.vmware.plugins.module_utils._module_pyvmomi_base import (
+    ModulePyvmomiBase
+)
+
+
+class VmwareGuestPowerstateModule(ModulePyvmomiBase):
+    def __init__(self, module):
+        super(VmwareGuestPowerstateModule, self).__init__(module)
+
+        self.result = dict(changed=False, result={})
+
+    def get_vm(self):
+        """
+        Finds and gets VM according to the specified name/uuid/moid
+        Returns:
+            tuple, vm object and PyVmomi object if found 
+        """
+        if self.module.params['folder']:
+            self.module.params['folder'] = self.module.params['folder'].rstrip('/')
+
+        pyv = PyVmomi(self.module)
+
+        vm = pyv.get_vm()
+
+        if not vm:
+            id = self.module.params.get('uuid') or self.module.params.get('moid') or self.module.params.get('name')
+            self.module.fail_json(msg="Unable to set power state for non-existing virtual machine : '%s'" % id)
+
+        return (vm, pyv)
+
+    def configure_vm_powerstate(self, vm, pyv):
+        """
+        Configures a VMs powerstate
+        """
+        if not self.current_state_matches_desired_state(vm, pyv):  
+            scheduled_at = self.module.params.get('scheduled_at', None)
+            if scheduled_at:
+                self.configure_vm_scheduled_powerstate(vm, pyv) #TODO: does this function actually change the state?
+            else:
+                # Check if a virtual machine is locked by a question
+                if check_answer_question_status(vm) and self.module.params['answer']:
+                    self.configure_vm_answerable_powerstate(vm, pyv) #TODO: does this function actually change the state?
+                else:
+                    self.result = set_vm_power_state(pyv.content, vm, self.module.params['state'], self.module.params['force'], self.module.params['state_change_timeout'],
+                                                self.module.params['answer'])
+                    del self.result["instance"]
+                self.result['result']['answer'] = self.module.params['answer']
+        
+        self.result['result']['moid'] = vm._GetMoId()
+        self.result['result']['name'] = vm.name
+        self.result['result']['instance'] = gather_vm_facts(pyv.content, vm)
+
+    def configure_vm_scheduled_powerstate(self, vm, pyv):
+        """
+        Configures a VM powerstate when scheduled task option is set
+        """
+        if not pyv.is_vcenter():
+            self.module.fail_json(msg="Scheduling task requires vCenter, hostname %s "
+                                "is an ESXi server." % self.module.params.get('hostname'))
+        powerstate = {
+            'present': vim.VirtualMachine.PowerOn,
+            'powered-off': vim.VirtualMachine.PowerOff,
+            'powered-on': vim.VirtualMachine.PowerOn,
+            'reboot-guest': vim.VirtualMachine.RebootGuest,
+            'restarted': vim.VirtualMachine.Reset,
+            'shutdown-guest': vim.VirtualMachine.ShutdownGuest,
+            'suspended': vim.VirtualMachine.Suspend,
+        }
+        try:
+            scheduled_date = datetime.strptime(scheduled_at, '%d/%m/%Y %H:%M')
+        except ValueError as e:
+            self.module.fail_json(msg="Failed to convert given date and time string to Python datetime object,"
+                                "please specify string in 'dd/mm/yyyy hh:mm' format: %s" % to_native(e))
+        schedule_task_spec = vim.scheduler.ScheduledTaskSpec()
+        schedule_task_spec.name = self.module.params['schedule_task_name'] or 'task_%s' % str(randint(10000, 99999))
+        schedule_task_spec.description = self.module.params['schedule_task_description'] or 'Schedule task for vm %s for ' \
+                                                'operation %s at %s' % (vm.name, self.module.params['state'], scheduled_at)
+        schedule_task_spec.scheduler = vim.scheduler.OnceTaskScheduler()
+        schedule_task_spec.scheduler.runAt = scheduled_date
+        schedule_task_spec.action = vim.action.MethodAction()
+        schedule_task_spec.action.name = powerstate[self.module.params['state']]
+        schedule_task_spec.enabled = self.module.params['schedule_task_enabled']
+
+        try:
+            pyv.content.scheduledTaskManager.CreateScheduledTask(vm, schedule_task_spec)
+            # As this is async task, we create scheduled task and mark state to changed.
+            self.result['changed'] = True
+        except vim.fault.InvalidName as e:
+            self.module.fail_json(msg="Failed to create scheduled task %s for %s : %s" % (self.module.params.get('state'),
+                                                                                    vm.name,
+                                                                                    to_native(e.msg)))
+        except vim.fault.DuplicateName as e:
+            self.module.fail_json(msg="Failed to create scheduled task %s as specified task "
+                                "name is invalid: %s" % (self.module.params.get('state'),
+                                                            to_native(e.msg)))
+        except vmodl.fault.InvalidArgument as e:
+            self.module.fail_json(msg="Failed to create scheduled task %s as specifications "
+                                "given are invalid: %s" % (self.module.params.get('state'),
+                                                            to_native(e.msg)))
+            
+    def configure_vm_answerable_powerstate(self, vm, pyv):
+        """
+        Configures a VM powerstate when answer option is set
+        """
+        try:
+            responses = make_answer_response(vm, self.module.params['answer'])
+            answer_question(vm, responses)
+        except Exception as e:
+            self.module.fail_json(msg="%s" % e)
+
+        # Wait until a virtual machine is unlocked
+        while True:
+            if check_answer_question_status(vm) is False:
+                break
+
+        self.result['changed'] = True
+    
+    def current_state_matches_desired_state(self, vm, pyv):
+        """
+        Checks the hosts current power state and compares it to the desired power state setting.
+        Returns:
+            bool, true if they match, otherwise false
+        """
+        facts = gather_vm_facts(pyv.content, vm)
+        state = self.module.params['state']
+        if state == 'present':
+            state = 'poweredon'
+        desired_state = state.replace('_', '').replace('-', '').lower()
+        current_state = facts['hw_power_status'].lower()
+
+        if current_state == desired_state:
+            return True
+        
+        return False
+
 
 def main():
     argument_spec = vmware_argument_spec()
     argument_spec.update(
-        datacenter=dict(type='str', default='ha-datacenter'),
+        datacenter=dict(type='str', required=True),
         state=dict(type='str', default='present',
                    choices=['present', 'powered-off', 'powered-on', 'reboot-guest', 'restarted', 'shutdown-guest', 'suspended']),
         name=dict(type='str'),
@@ -235,14 +367,15 @@ def main():
         uuid=dict(type='str'),
         moid=dict(type='str'),
         use_instance_uuid=dict(type='bool', default=False),
-        folder=dict(type='str'),
+        folder=dict(type='str', required=False),
         force=dict(type='bool', default=False),
-        scheduled_at=dict(type='str'),
-        schedule_task_name=dict(),
-        schedule_task_description=dict(),
+        scheduled_at=dict(type='str', required=False),
+        schedule_task_name=dict(type='str', required=False),
+        schedule_task_description=dict(type='str', required=False),
         schedule_task_enabled=dict(type='bool', default=True),
         state_change_timeout=dict(type='int', default=0),
         answer=dict(type='list',
+                    required=False,
                     elements='dict',
                     options=dict(
                         question=dict(type='str', required=True),
@@ -262,90 +395,11 @@ def main():
         ],
     )
 
-    result = dict(changed=False,)
-
-    if module.params['folder']:
-        module.params['folder'] = module.params['folder'].rstrip('/')
-
-    pyv = PyVmomi(module)
-
-    # Check if the VM exists before continuing
-    vm = pyv.get_vm()
-
-    if vm:
-        # VM already exists, so set power state
-        scheduled_at = module.params.get('scheduled_at', None)
-        if scheduled_at:
-            if not pyv.is_vcenter():
-                module.fail_json(msg="Scheduling task requires vCenter, hostname %s "
-                                     "is an ESXi server." % module.params.get('hostname'))
-            powerstate = {
-                'present': vim.VirtualMachine.PowerOn,
-                'powered-off': vim.VirtualMachine.PowerOff,
-                'powered-on': vim.VirtualMachine.PowerOn,
-                'reboot-guest': vim.VirtualMachine.RebootGuest,
-                'restarted': vim.VirtualMachine.Reset,
-                'shutdown-guest': vim.VirtualMachine.ShutdownGuest,
-                'suspended': vim.VirtualMachine.Suspend,
-            }
-            dt = ''
-            try:
-                dt = datetime.strptime(scheduled_at, '%d/%m/%Y %H:%M')
-            except ValueError as e:
-                module.fail_json(msg="Failed to convert given date and time string to Python datetime object,"
-                                     "please specify string in 'dd/mm/yyyy hh:mm' format: %s" % to_native(e))
-            schedule_task_spec = vim.scheduler.ScheduledTaskSpec()
-            schedule_task_name = module.params['schedule_task_name'] or 'task_%s' % str(randint(10000, 99999))
-            schedule_task_desc = module.params['schedule_task_description']
-            if schedule_task_desc is None:
-                schedule_task_desc = 'Schedule task for vm %s for ' \
-                                     'operation %s at %s' % (vm.name, module.params['state'], scheduled_at)
-            schedule_task_spec.name = schedule_task_name
-            schedule_task_spec.description = schedule_task_desc
-            schedule_task_spec.scheduler = vim.scheduler.OnceTaskScheduler()
-            schedule_task_spec.scheduler.runAt = dt
-            schedule_task_spec.action = vim.action.MethodAction()
-            schedule_task_spec.action.name = powerstate[module.params['state']]
-            schedule_task_spec.enabled = module.params['schedule_task_enabled']
-
-            try:
-                pyv.content.scheduledTaskManager.CreateScheduledTask(vm, schedule_task_spec)
-                # As this is async task, we create scheduled task and mark state to changed.
-                module.exit_json(changed=True)
-            except vim.fault.InvalidName as e:
-                module.fail_json(msg="Failed to create scheduled task %s for %s : %s" % (module.params.get('state'),
-                                                                                         vm.name,
-                                                                                         to_native(e.msg)))
-            except vim.fault.DuplicateName as e:
-                module.exit_json(changed=False, details=to_native(e.msg))
-            except vmodl.fault.InvalidArgument as e:
-                module.fail_json(msg="Failed to create scheduled task %s as specifications "
-                                     "given are invalid: %s" % (module.params.get('state'),
-                                                                to_native(e.msg)))
-        else:
-            # Check if a virtual machine is locked by a question
-            if check_answer_question_status(vm) and module.params['answer']:
-                try:
-                    responses = make_answer_response(vm, module.params['answer'])
-                    answer_question(vm, responses)
-                except Exception as e:
-                    module.fail_json(msg="%s" % e)
-
-                # Wait until a virtual machine is unlocked
-                while True:
-                    if check_answer_question_status(vm) is False:
-                        break
-
-                result['changed'] = True
-                result['instance'] = gather_vm_facts(pyv.content, vm)
-            else:
-                result = set_vm_power_state(pyv.content, vm, module.params['state'], module.params['force'], module.params['state_change_timeout'],
-                                            module.params['answer'])
-            result['answer'] = module.params['answer']
-    else:
-        id = module.params.get('uuid') or module.params.get('moid') or module.params.get('name')
-        module.fail_json(msg="Unable to set power state for non-existing virtual machine : '%s'" % id)
-
+    vmware_guest_powerstate = VmwareGuestPowerstateModule(module)
+    vm, pyv = vmware_guest_powerstate.get_vm()
+    vmware_guest_powerstate.configure_vm_powerstate(vm, pyv)
+    result = vmware_guest_powerstate.result
+    
     if result.get('failed') is True:
         module.fail_json(**result)
 
