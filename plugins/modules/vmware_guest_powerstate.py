37456d4f1e6d3a9a925eb4cfaead3e4dd6ccf779
Orr Ben Galim <obengali@redhat.com>
2025-02-12 18:34:23 +0200

    vars to dir

--- plugins/modules/vmware_guest_powerstate.py
+++ plugins/modules/vmware_guest_powerstate.py
@@ -272,7 +272,7 @@ class VmwareGuestPowerstateModule(ModulePyvmomiBase):
             first vm object found or None if no matches were found 
         """
         vm_list = self.get_vm_using_params(fail_on_missing=True)
-        self.module.fail_json(msg="%s" % vars(vm_list))
+        self.module.fail_json(msg="%s" % dir(vm_list))
         first_vm = vm_list[0]
         return first_vm
     
