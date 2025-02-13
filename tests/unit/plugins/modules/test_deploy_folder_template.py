6a93af3834a49e6d35f8185d5fc34fcd36dfe123
Mike Morency <mikemorency93@gmail.com>
2025-02-13 16:03:55 -0500

    create module to deploy vm from folder

--- tests/unit/plugins/modules/test_deploy_folder_template.py
+++ tests/unit/plugins/modules/test_deploy_folder_template.py
@@ -4,8 +4,8 @@ __metaclass__ = type
 import sys
 import pytest
 
-from ansible_collections.vmware.vmware.plugins.modules.deploy_content_library_ovf import (
-    VmwareContentDeployOvf,
+from ansible_collections.vmware.vmware.plugins.modules.deploy_folder_template import (
+    VmwareFolderTemplate,
     main as module_main
 )
 from ansible_collections.vmware.vmware.plugins.module_utils.clients._pyvmomi import (
@@ -26,31 +26,33 @@ pytestmark = pytest.mark.skipif(
 )
 
 
-class TestDeployContentLibraryOvf(ModuleTestCase):
+class TestVmwareFolderTemplate(ModuleTestCase):
 
     def __prepare(self, mocker):
         mocker.patch.object(PyvmomiClient, 'connect_to_api', return_value=(mocker.Mock(), mocker.Mock()))
         mocker.patch.object(VmwareRestClient, 'connect_to_api', return_value=mocker.Mock())
         self.test_vm = MockVmwareObject(name="test")
+        self.test_template = mocker.Mock()
+        self.test_template.config.template = True
 
-        mocker.patch.object(VmwareContentDeployOvf, 'get_resource_pool_by_name_or_moid', return_value=MockVmwareObject())
-        mocker.patch.object(VmwareContentDeployOvf, 'get_datastore_by_name_or_moid', return_value=MockVmwareObject())
-        mocker.patch.object(VmwareContentDeployOvf, 'get_datacenter_by_name_or_moid', return_value=MockVmwareObject())
-        mocker.patch.object(VmwareContentDeployOvf, 'deploy', return_value=self.test_vm._GetMoId())
+        mocker.patch.object(VmwareFolderTemplate, 'get_folder_by_absolute_path', return_value=MockVmwareObject())
+        mocker.patch.object(VmwareFolderTemplate, 'get_objs_by_name_or_moid', return_value=[self.test_template])
+        mocker.patch.object(VmwareFolderTemplate, 'get_resource_pool_by_name_or_moid', return_value=MockVmwareObject())
+        mocker.patch.object(VmwareFolderTemplate, 'get_datastore_by_name_or_moid', return_value=MockVmwareObject())
+        mocker.patch.object(VmwareFolderTemplate, 'get_datacenter_by_name_or_moid', return_value=MockVmwareObject())
+        mocker.patch.object(VmwareFolderTemplate, 'deploy', return_value=self.test_vm)
 
     def test_present(self, mocker):
         self.__prepare(mocker)
-        mocker.patch.object(VmwareContentDeployOvf, 'get_deployed_vm', return_value=None)
+        mocker.patch.object(VmwareFolderTemplate, 'get_deployed_vm', return_value=None)
         set_module_args(
             hostname="127.0.0.1",
             username="administrator@local",
             password="123456",
             add_cluster=False,
             vm_name=self.test_vm.name,
-            library_item_id='111',
-            datastore='foo',
+            template_name="foo",
             datacenter='foo',
-            resource_pool='foo'
         )
 
         with pytest.raises(AnsibleExitJson) as c:
@@ -59,17 +61,15 @@ class TestDeployContentLibraryOvf(ModuleTestCase):
         assert c.value.args[0]["changed"] is True
         assert c.value.args[0]["vm"]["moid"] is self.test_vm._GetMoId()
 
-        mocker.patch.object(VmwareContentDeployOvf, 'get_deployed_vm', return_value=self.test_vm)
+        mocker.patch.object(VmwareFolderTemplate, 'get_deployed_vm', return_value=self.test_vm)
         set_module_args(
             hostname="127.0.0.1",
             username="administrator@local",
             password="123456",
             add_cluster=False,
             vm_name=self.test_vm.name,
-            library_item_id='111',
-            datastore='foo',
+            template_name="foo",
             datacenter='foo',
-            resource_pool='foo'
         )
 
         with pytest.raises(AnsibleExitJson) as c:
