4ea78f2a045d8fb1bb10b1d8d3e83f650ec69560
Mike Morency <mikemorency93@gmail.com>
2025-02-19 10:16:18 -0500

    increase test coverage

--- tests/unit/plugins/module_utils/test_module_pyvmomi_base.py
+++ tests/unit/plugins/module_utils/test_module_pyvmomi_base.py
@@ -35,4 +35,26 @@ class TestModulePyvmomiBase():
         mock_prop.val = 'test1'
         mock_mor.propSet = [mock_prop]
         mocker.patch.object(self.base, 'get_managed_object_references', return_value=[mock_mor])
-        assert self.base.get_objs_by_name_or_moid(vim.VirtualMachine, mock_prop.val)
+        assert self.base.get_objs_by_name_or_moid(vim.VirtualMachine, mock_prop.val)[0]._GetMoId() == mock_mor.obj._moid
+
+        # test slightly different inputs
+        assert self.base.get_objs_by_name_or_moid([vim.VirtualMachine], mock_prop.val, return_all=True, search_root_folder=mocker.Mock())[0]._GetMoId() == mock_mor.obj._moid
+
+        # test matching moid instead of name
+        assert self.base.get_objs_by_name_or_moid(vim.VirtualMachine, mock_mor.obj._GetMoId())[0]._GetMoId() == mock_mor.obj._moid
+
+        # test no moids found
+        mocker.patch.object(self.base, 'get_managed_object_references', return_value=[])
+        assert self.base.get_objs_by_name_or_moid(vim.VirtualMachine, mock_prop.val) == []
+
+        # test no matching identifier found
+        mock_mor_2 = mocker.Mock()
+        mock_mor_2.obj = create_mock_vsphere_object()
+        mock_prop_2 = mocker.Mock()
+        mock_prop_2.name = 'name'
+        mock_prop_2.val = 'test2'
+        mock_mor_2.propSet = [mock_prop_2]
+        mocker.patch.object(self.base, 'get_managed_object_references', return_value=[mock_mor_2])
+        assert self.base.get_objs_by_name_or_moid(vim.VirtualMachine, mock_prop.val) == []
+
+
