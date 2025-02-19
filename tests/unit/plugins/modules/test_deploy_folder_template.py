fce14b2e400854cebbb21e646c501aaa0d1d47c9
Mike Morency <mikemorency93@gmail.com>
2025-02-19 10:16:18 -0500

    increase test coverage

--- tests/unit/plugins/modules/test_deploy_folder_template.py
+++ tests/unit/plugins/modules/test_deploy_folder_template.py
@@ -14,8 +14,8 @@ from ansible_collections.vmware.vmware.plugins.module_utils.clients._pyvmomi imp
 from ansible_collections.vmware.vmware.plugins.module_utils.clients._rest import (
     VmwareRestClient
 )
-from ...common.utils import (
-    AnsibleExitJson, ModuleTestCase, set_module_args,
+from .common.utils import (
+    AnsibleExitJson, AnsibleFailJson, ModuleTestCase, set_module_args,
 )
 from ...common.vmware_object_mocks import (
     MockVmwareObject
@@ -44,6 +44,7 @@ class TestVmwareFolderTemplate(ModuleTestCase):
 
     def test_present(self, mocker):
         self.__prepare(mocker)
+        # test template_name
         mocker.patch.object(VmwareFolderTemplate, 'get_deployed_vm', return_value=None)
         set_module_args(
             hostname="127.0.0.1",
@@ -61,6 +62,22 @@ class TestVmwareFolderTemplate(ModuleTestCase):
         assert c.value.args[0]["changed"] is True
         assert c.value.args[0]["vm"]["moid"] is self.test_vm._GetMoId()
 
+        # test template_id
+        mocker.patch.object(VmwareFolderTemplate, 'get_deployed_vm', return_value=None)
+        set_module_args(
+            hostname="127.0.0.1",
+            username="administrator@local",
+            password="123456",
+            add_cluster=False,
+            vm_name=self.test_vm.name,
+            template_id="foo",
+            datacenter='foo',
+        )
+
+        with pytest.raises(AnsibleExitJson) as c:
+            module_main()
+
+        # test no change
         mocker.patch.object(VmwareFolderTemplate, 'get_deployed_vm', return_value=self.test_vm)
         set_module_args(
             hostname="127.0.0.1",
@@ -77,3 +94,24 @@ class TestVmwareFolderTemplate(ModuleTestCase):
 
         assert c.value.args[0]["changed"] is False
         assert c.value.args[0]["vm"]["moid"] is self.test_vm._GetMoId()
+
+
+    def test_template_error(self, mocker):
+        self.__prepare(mocker)
+        mocker.patch.object(VmwareFolderTemplate, 'get_deployed_vm', return_value=None)
+        mocker.patch.object(VmwareFolderTemplate, 'get_objs_by_name_or_moid', return_value=None)
+        set_module_args(
+            hostname="127.0.0.1",
+            username="administrator@local",
+            password="123456",
+            add_cluster=False,
+            vm_name=self.test_vm.name,
+            template_id="foo",
+            datacenter='foo',
+        )
+
+        with pytest.raises(AnsibleFailJson) as c:
+            module_main()
+
+        assert c.value.args[0]["msg"].startswith('Unable to find template with ID')
+        assert c.value.args[0]["failed"] is True
