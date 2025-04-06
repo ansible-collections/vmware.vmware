37ee833a8dd1fef99d8fb40e34c871f9abefb0c3
OrrBG <obengali@redhat.com>
2025-04-06 14:52:28 +0300

    argument_spec module renamed

--- plugins/modules/vm_snapshot.py
+++ plugins/modules/vm_snapshot.py
@@ -242,7 +242,7 @@ except ImportError:
 from ansible_collections.vmware.vmware.plugins.module_utils._module_pyvmomi_base import (
     ModulePyvmomiBase
 )
-from ansible_collections.vmware.vmware.plugins.module_utils._vmware_argument_spec import (
+from ansible_collections.vmware.vmware.plugins.module_utils.argument_spec import (
     base_argument_spec
 )
 from ansible_collections.vmware.vmware.plugins.module_utils._vmware_tasks import RunningTaskMonitor
