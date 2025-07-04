9dae8ce5a6eff92b3da6b0bfb194c2c12e2a7c45
Mike Morency <mikemorency93@gmail.com>
2025-07-03 20:57:52 -0400

    checking in more changes

--- plugins/module_utils/vm/devices/disks/_handler.py
+++ plugins/module_utils/vm/devices/disks/_handler.py
@@ -1,9 +1,10 @@
 from abc import abstractmethod
 from ansible_collections.vmware.vmware.plugins.module_utils.vm._abstracts import ParameterHandlerBase
-from ansible_collections.vmware.vmware.plugins.module_utils.vm.devices.disks.disk import Disk
+from ansible_collections.vmware.vmware.plugins.module_utils.vm.devices.disks._disk import Disk
+from ansible_collections.vmware.vmware.plugins.module_utils.vm.devices._utils import parse_device_node
 
 
-class DiskParameterHandlerBase(ParameterHandlerBase):
+class DiskParameterHandler(ParameterHandlerBase):
     def __init__(self, vm, module, controller_handlers):
         super().__init__(vm, module)
 
@@ -37,7 +38,26 @@ class DiskParameterHandlerBase(ParameterHandlerBase):
 
     def _parse_disk_params(self):
         for disk_param in self.module.params.get("disks", []):
-            disk = Disk(disk_param)
+            controller_type, controller_bus_number, unit_number = parse_device_node(disk_param['device_node'])
+            for controller_handler in self.controller_handlers:
+                if controller_type == controller_handler.category:
+                    controller = controller_handler.controllers.get(controller_bus_number)
+                    break
+
+            if controller is None:
+                raise self.module.fail_json(
+                    msg="No controller has been configured for device %s." % disk_param['device_node'],
+                    device_node=disk_param['device_node'],
+                    available_controllers=[c.name_as_str for c in controller_handler.controllers.values()]
+                )
+
+            disk = Disk(
+                size=disk_param.get("size"),
+                backing=disk_param.get("backing"),
+                mode=disk_param.get("mode"),
+                controller=controller,
+                unit_number=unit_number
+            )
             self.disks.append(disk)
 
     def update_config_spec_with_params(self, configspec):
@@ -46,3 +66,6 @@ class DiskParameterHandlerBase(ParameterHandlerBase):
                 configspec.deviceChange.append(disk.create_disk_spec())
             else:
                 configspec.deviceChange.append(disk.update_disk_spec())
+
+    def get_params_requiring_power_cycle(self):
+        return set()
