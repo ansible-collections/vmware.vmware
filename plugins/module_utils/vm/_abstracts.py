862c7032a89a55e0bc11bf135c887625d018a92c
Mike Morency <mikemorency93@gmail.com>
2025-07-04 20:45:43 -0400

    updates

--- plugins/module_utils/vm/_abstracts.py
+++ plugins/module_utils/vm/_abstracts.py
@@ -1,4 +1,48 @@
 from abc import ABC, abstractmethod
+import functools
+
+from ansible_collections.vmware.vmware.plugins.module_utils.vm.errors import PowerCycleRequiredError
+
+
+class ParameterChangeSet():
+    def __init__(self, vm, params):
+        self.vm = vm
+        self.params = params
+        if vm is None:
+            raise ValueError("VM object is None, but it is required to check if a change is required.")
+
+        self.changes_required = False
+        self.power_cycle_required = False
+
+    def check_if_change_is_required(self, parameter_name, vm_attribute, power_sensitive=False):
+        self._check_if_param_differs_from_vm(parameter_name, vm_attribute)
+        if power_sensitive:
+            self._check_if_change_violates_power_state(parameter_name)
+
+    def _check_if_param_differs_from_vm(self, parameter_name, vm_attribute):
+        if self.params.get(parameter_name) is None or self.changes_required:
+            return
+
+        if self.params.get(parameter_name) == functools.reduce(getattr, vm_attribute.split('.'), self.vm):
+            return
+
+        self.changes_required = True
+
+    def _check_if_change_violates_power_state(self, parameter_name):
+        if self.vm.runtime.powerState != 'poweredOn' or not self.changes_required:
+            return
+
+        if self.params.get('power_cycle_allowed'):
+            self.power_cycle_required = True
+        else:
+            raise PowerCycleRequiredError(parameter_name)
+
+    def combine(self, change_set):
+        if not isinstance(change_set, ParameterChangeSet):
+            raise ValueError("change_set must be an instance of ParameterChangeSet")
+
+        self.changes_required = self.changes_required or change_set.changes_required
+        self.power_cycle_required = self.power_cycle_required or change_set.power_cycle_required
 
 
 class ConfiguratorBase(ABC):
@@ -10,7 +54,7 @@ class ConfiguratorBase(ABC):
         self.vm = vm
         self.module = module
         self.params = module.params
-        self.required_changes = None
+        self.pending_changes = None
 
     @abstractmethod
     def validate_params_for_creation(self):
@@ -50,7 +94,7 @@ class ParameterHandlerBase(ABC):
         self.vm = vm
 
     @abstractmethod
-    def validate_params_for_creation(self):
+    def verify_parameter_constraints(self):
         """
         Validate parameters for creation.
         Returns:
@@ -59,18 +103,7 @@ class ParameterHandlerBase(ABC):
         pass
 
     @abstractmethod
-    def validate_params_for_reconfiguration(self):
-        """
-        Validate parameters for reconfiguration. Reconfiguration may require that the VM is powered off,
-        and this method should validate that the VM is in the correct state or the user has allowed power
-        cycling.
-        Returns:
-            None, the module will fail with an error if the parameters are invalid.
-        """
-        pass
-
-    @abstractmethod
-    def update_config_spec_with_params(self, configspec):
+    def populate_config_spec_with_parameters(self, configspec):
         """
         Update a config spec with parameters for this handler. This should map module params to
         configspec only if they are specified by the user. For example, if the user does not specify
@@ -83,7 +116,7 @@ class ParameterHandlerBase(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def params_differ_from_actual_config(self, vsphere_obj):
+    def get_parameter_change_set(self):
         """
         Check if current VM config differs from desired config. This should not validate params
         or communicate what values are different. It should only check if there are any differences; it
