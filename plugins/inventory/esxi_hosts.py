cb78718638eaed02ea41c270ee4037a96503de00
chaeynz <97678748+chaeynz@users.noreply.github.com>
2025-11-10 20:34:20 +0100

    docs: Update example inventory file name in esxi_hosts.py (#277)

--- plugins/inventory/esxi_hosts.py
+++ plugins/inventory/esxi_hosts.py
@@ -34,8 +34,8 @@ options:
 
 EXAMPLES = r"""
 # Below are examples of inventory configuration files that can be used with this plugin.
-# To test these and see the resulting inventory, save the snippet in a file named hosts.vmware_esxi.yml and run:
-# ansible-inventory -i hosts.vmware_esxi.yml --list
+# To test these and see the resulting inventory, save the snippet in a file named esxi_hosts.yml and run:
+# ansible-inventory -i esxi_hosts.yml --list
 
 
 # Simple configuration with in-file authentication parameters
